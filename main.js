aea2ff19444f598fb42c13b95931bcc886af0d9f
KefineKefine <u23rn6m3@gmail.com>
2025-03-28 16:59:52 +0800

    background finished

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const blob = document.getElementById("blob");
 
-window.onpointermove = event => { 
-  const { pageX, pageY } = event; // Tracks position beyond viewport height
 
-  blob.animate({
-    left: `${pageX}px`,
-    top: `${pageY}px`
-  }, { duration: 9000, fill: "forwards" });
-};
+//const blob = document.getElementById("blob");
+
+//window.onpointermove = event => { 
+//  const { pageX, pageY } = event; // Tracks position beyond viewport height
+//
+//  blob.animate({
+//    left: `${pageX}px`,
+//    top: `${pageY}px`
+//  }, { duration: 1000, fill: "forwards" });
+//};
