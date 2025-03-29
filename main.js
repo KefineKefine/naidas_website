1614fe414606bc5493ca7534bc929a7fb15c83d3
KefineKefine <u23rn6m3@gmail.com>
2025-03-29 16:37:32 +0800

    major changes LOL

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-//const blob = document.getElementById("blob");
+const blob = document.getElementById("blob");
 
-//window.onpointermove = event => { 
-//  const { pageX, pageY } = event; // Tracks position beyond viewport height
-//
-//  blob.animate({
-//    left: `${pageX}px`,
-//    top: `${pageY}px`
-//  }, { duration: 1000, fill: "forwards" });
-//};
+window.onpointermove = event => { 
+  const { pageX, pageY } = event; // Tracks position beyond viewport height
+
+  blob.animate({
+    left: `${pageX}px`,
+    top: `${pageY}px`
+  }, { duration: 1000, fill: "forwards" });
+};
