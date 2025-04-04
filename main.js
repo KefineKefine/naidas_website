01c1e3e06985adfe97ef735c8b485be27a1b7c38
KefineKefine <u23rn6m3@gmail.com>
2025-04-04 21:20:46 +0800

    lmao

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 const blob = document.getElementById("blob");
 const eril = document.getElementById("eril");
 
+
+vis_prompts = false
+
 window.onpointermove = event => { 
   const { pageX, pageY } = event; // Tracks position beyond viewport height
 
@@ -12,3 +15,28 @@ window.onpointermove = event => {
   }, { duration: 1000, fill: "forwards" });
 
 };
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const main_image = document.getElementById("main-img");
+  const button_container = document.getElementById("button-container")
+
+  for (let button of button_container.children) {
+    button.style.display = 'none';
+  }
+
+  button_container.style.display = 'none';
+
+  if (main_image) {
+    main_image.addEventListener("click", function() {
+      
+      for (let button of button_container.children) {
+        button.style.display = (button.style.display === 'none') ? 'inline-flex' : 'none';
+      }
+
+      button_container.style.display = (button_container.style.display === 'none') ? 'flex' : 'none';
+
+      });
+  }
+});
